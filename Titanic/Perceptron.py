c9fbeba0f5aec09a5ae32b671fcd1350319a60c7
DLuscombe <148010306+DLuscombe@users.noreply.github.com>
2024-08-05 14:40:13 +0100

    perceptron1

--- Titanic/Perceptron.py
+++ Titanic/Perceptron.py
@@ -1,6 +1,47 @@
 import pandas as pd
 import numpy as np
 
-def start_perceptron(X, y, w, lr, max_iter):
-    return 0
-
+def start_perceptron(X, y, w, alpha, max_iter):
+    run = True
+    count = 0
+    updates = 0
+    iteration = 0
+    total = len(X) 
+    print(total)
+    
+    while run:
+        if count >= total:
+            count = 0
+            if updates == 0:
+                run = False
+                print(f"Algorithm converged, final w = {w}")
+                print(f"Total number of iterations = {iteration}")
+                break
+            else:
+                updates = 0
+                
+        x_count = X[count]
+        yhat = 1 if np.dot(w, x_count) >= 0 else 0
+                
+        if y[count] < yhat:
+            w = w - alpha * x_count
+            count += 1
+            updates += 1
+            iteration += 1
+        elif y[count] > yhat:
+            w = w + alpha * x_count
+            count += 1
+            updates += 1
+            iteration += 1
+        else:
+            count += 1
+            iteration += 1      
+        
+        if iteration == max_iter:
+            run = False
+            print(f"Algorithm did not converge, final w = {w}")
+            print(f"Total number of iterations = {iteration}")
+            break
+            
+    return w
+            
--- Titanic/Titanic.py
+++ Titanic/Titanic.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
+import Perceptron as p
 
 # Load the dataset
 data_path = "/Users/DLuscombe/Library/Mobile Documents/com~apple~CloudDocs/Northeastern/Projects/AIExperiments/Titanic/Dataset/"
 df = pd.read_csv(data_path + 'train.csv')
+df_test = pd.read_csv(data_path + 'test.csv')
 
 # Data Preprocessing
 #remove columns that are not useful
@@ -21,7 +23,36 @@ avg_fare = df.loc[:, 'Fare'].mean()
 
 #init data to matrix and vector values
 w_vector = np.array([-1, 2, 1, avg_age, avg_fare])
-X_matrix = np.array(df.iloc[1:,0:6])
 y_vector = df['Survived'].to_numpy()
+X_matrix = np.array(df.drop(columns='Survived').iloc[:,0:6])
 
+#run perceptron
+alpha = 0.08
+max_iter = 10000000
+w = p.start_perceptron(X_matrix, y_vector, w_vector, alpha, max_iter)
 
+# Perceptron prediction function
+def perceptron_predict(X, w):
+    return np.where(np.dot(X, w) >= 0.0, 1, 0)
+
+# Calculate accuracy
+def calculate_accuracy(X, y, w):
+    predictions = perceptron_predict(X, w)
+    accuracy = np.mean(predictions == y)
+    return accuracy
+
+# Test the accuracy
+accuracy = calculate_accuracy(X_matrix, y_vector, w)
+print("Accuracy:", accuracy)
+
+# Data Preprocessing for test data
+final_submission = df_test['PassengerId']
+#remove columns that are not useful
+df_test = df_test.drop(columns=['Name', 'Ticket', 'Cabin', 'Embarked', 'PassengerId', 'Parch', 'SibSp'])
+#add bias column
+df_test.insert(loc=1, column='bias', value=1)
+# Using map
+df_test['Sex'] = df['Sex'].map({'male': 1, 'female': 0})
+
+print(df.head)
+print(df_test.head)
